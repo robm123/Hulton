2f0c1185834d4d2ac13ed8f44372a57fcc62503b
monmon-2007 <minamonir11@hotmail.com>
2017-04-26 20:15:58 -0400

    Rob Is Gay

--- login.php
+++ login.php
@@ -1,22 +1,37 @@
 <?php
+	$country = $_POST["country"];
+	$state = $_POST["state"];
+	$checkin = $_POST["checkin"];
+	$checkout = $_POST["checkout"];
+	$type = $_POST["type"];
 
 	if(isset($_POST['search']))
 	{
-		$dbhost = 'www.db4free.net';
-		$dbuser = '*********';
-		$dbpass = '*********';
-      	$dbname = '*********';
+		$dbhost = 'robertomina123.cnfw63tusvga.us-west-2.rds.amazonaws.com';
+		$dbuser = 'RobertoMina123';
+		$dbpass = 'robminapassword';
+  	$dbname = 'Hulton';
 		$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname); // Create connection
 		if(! $conn )
-      	{
+    {
          die('Could not connect: ' . mysql_error());
-      	}
+    }
+
+		$sql ="SELECT H.ID FROM Hotels H, ROOM R WHERE H.State = '$state' AND H.Country = '$country' AND R.type = '$type' AND H.ID = R.hotelID ";
+		$result = $conn->query($sql);
 
 
-		$country = $_POST["country"];
-		$state = $_POST["state"];
-		$checkin = $_POST["checkin"];
-		$checkout = $_POST["checkout"];
-		$type = $_POST["type"];
+		while($row = mysqli_fetch_assoc($result))
+		{
+			if( $row["State"] == "New York")
+			echo "string";
+		}
+
+
+
+
+
+
+
 	}
 ?>
